1298695b71100b18c5da66e3f5afe381c66e6247
boypht <boypht91@gmail.com>
2013-04-02 08:48:06 +0700

    content of email

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -2,6 +2,6 @@ class AdminMailer < ActionMailer::Base
   default from: "admin@example.com"
 
   def send_email(email, pass, link)
-  	mail(:to => email, :subject => "Welcome to exam online", :body => "Your password: "+pass+"\nLink: <a>"+link+"</a>")
+  	mail(:to => email, :subject => "Welcome to exam online", :body => "Your password: "+pass+"\nLink: "+link)
   end
 end
